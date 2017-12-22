6a74c400974e00a4619ff47b50815882483b2bb4
Long <longhui.yang@inveno.cn>
2017-12-22 18:34:37 +0800

    全局常量定义

---
+++ eastpoker-fr-const/src/com/poker/cmd/AccessCmd.java
@@ -0,0 +1,8 @@
+package com.poker.cmd;
+
+import com.poker.server.Server;
+
+public final class AccessCmd {
+	
+	public static final int CMD_CLIENT_HEAR_BEAT = (Server.SERVER_ACCESS <<16) + 1;//0x20001
+}
---
+++ eastpoker-fr-const/src/com/poker/cmd/UserCmd.java
@@ -0,0 +1,5 @@
+package com.poker.cmd;
+
+public class UserCmd {
+
+}
