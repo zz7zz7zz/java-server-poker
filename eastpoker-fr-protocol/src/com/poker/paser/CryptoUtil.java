6652d898b5ca717f02183965e3e9ffb56e0a30dc
Long <longhui.yang@inveno.cn>
2017-12-25 17:13:01 +0800

    解析类

---
+++ eastpoker-fr-protocol/src/com/poker/paser/CryptoUtil.java
@@ -0,0 +1,31 @@
+package com.poker.paser;
+
+/**
+ * author       :   Administrator
+ * created on   :   2017/12/25
+ * description  :
+ */
+
+public final class CryptoUtil {
+
+    public final static byte[] ENCRYPT_DATA = new byte[256];
+    public final static byte[] DECRYPT_DATA = new byte[256];
+
+    static{
+
+    };
+
+    public final static void encrypt(byte[] data){
+//        for(int i = 0;i<= data.length;i++){
+//            data[i]=ENCRYPT_DATA[i];
+//        }
+    }
+
+    public final static void decrypt(byte[] data){
+//        for(int i = 0;i<= data.length;i++){
+//            for(int i = 0;i<= data.length;i++){
+//                data[i]=DECRYPT_DATA[i];
+//            }
+//        }
+    }
+}
---
+++ eastpoker-fr-protocol/src/com/poker/paser/Packet.java
@@ -0,0 +1,78 @@
+package com.poker.paser;
+
+/**
+ * author       :   Administrator
+ * created on   :   2017/12/25
+ * description  :
+ */
+
+public class Packet {
+
+    public Header header;
+    public byte[] body;
+
+    //---------------------------------------------------------------------------------------------
+    public static class Header {
+
+        public static final int HEADER_LENGTH = 16;
+
+        public int      length;
+        public byte     version = 1;
+        public byte     encrypt;
+        public int      cmd;
+        public short    gid;
+
+        //-----------------------------------
+        public int getLength(byte[] data){
+            return 0;
+        }
+
+        public byte getVersion(byte[] data){
+            return 0;
+        }
+
+        public byte getEncrypt(byte[] data){
+            return 0;
+        }
+
+        public int getCmd(byte[] data){
+            return 0;
+        }
+
+        public short getGid(byte[] data){
+            return 0;
+        }
+
+        //-----------------------------------
+        public void setLength(int length){
+            this.length = length;
+        }
+
+        public void setVersion(byte version){
+            this.version = version;
+        }
+
+        public void setEncrypt(boolean isEncrypt){
+            this.encrypt = (byte)(isEncrypt ? 1 : 0);
+        }
+
+        public void setCmd(int cmd){
+            this.cmd = cmd;
+        }
+
+        public void setGid(short gid){
+            this.gid = gid;
+        }
+
+        //-----------------------------------
+        public static byte[] build(byte[] header , Header mHeader){
+
+            return null;
+        }
+
+        public static Header parse(byte[] data){
+
+            return null;
+        }
+    }
+}
