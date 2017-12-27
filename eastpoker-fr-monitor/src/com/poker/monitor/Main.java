cd4ef084b500f05efd81f6077df0fc52b3b3fe58
Long <longhui.yang@inveno.cn>
2017-12-27 17:02:12 +0800

    打印所有的服务

--- eastpoker-fr-monitor/src/com/poker/monitor/Main.java
+++ eastpoker-fr-monitor/src/com/poker/monitor/Main.java
@@ -2,10 +2,15 @@ package com.poker.monitor;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.open.net.server.GServer;
-import com.open.net.server.impl.udp.bio.UdpBioClient;
 import com.open.net.server.impl.udp.nio.UdpNioClient;
 import com.open.net.server.impl.udp.nio.UdpNioServer;
 import com.open.net.server.structures.AbstractClient;
@@ -17,7 +22,7 @@ import com.open.net.server.structures.message.Message;
 import com.open.util.log.Logger;
 import com.poker.data.DataPacket;
 import com.poker.protocols.server.ServerInfoProto;
-
+import com.poker.protocols.server.ServerInfoProto.ServerInfo;;
 /**
  * author       :   long
  * created on   :   2017/11/30
@@ -54,6 +59,8 @@ public class Main {
     }
 
     //-------------------------------------------------------------------------------------------
+    public static HashMap<Integer, ArrayList<ServerInfoProto.ServerInfo>> serverOnlineList = new HashMap<Integer, ArrayList<ServerInfoProto.ServerInfo>>();
+    
     public static AbstractMessageProcessor mMessageProcessor = new AbstractMessageProcessor() {
 
         private ByteBuffer mWriteBuffer  = ByteBuffer.allocate(128*1024);
@@ -62,8 +69,36 @@ public class Main {
         
         protected void onReceiveMessage(AbstractClient client, Message msg){
         	try {
-        		ServerInfoProto.ServerInfo server = ServerInfoProto.ServerInfo.parseFrom(msg.data,DataPacket.Header.HEADER_LENGTH+msg.offset,msg.length-DataPacket.Header.HEADER_LENGTH);
-        		Logger.v(server.toString());
+        		ServerInfo enterServer = ServerInfo.parseFrom(msg.data,DataPacket.Header.HEADER_LENGTH+msg.offset,msg.length-DataPacket.Header.HEADER_LENGTH);
+        		ArrayList<ServerInfo> serverArray = serverOnlineList.get(enterServer.getType());
+        		if(null == serverArray){
+        			serverArray = new ArrayList<ServerInfo>(10);
+        			serverOnlineList.put(enterServer.getType(), serverArray);
+        		}else{
+            		for(ServerInfo obj:serverArray){
+            			if(obj.getId() == enterServer.getId()){
+            				serverArray.remove(obj);
+            				break;
+            			}
+            		}
+        		}
+        		serverArray.add(enterServer);
+
+        		//打印所有的服务
+        		Iterator<Entry<Integer, ArrayList<ServerInfo>>> iter = serverOnlineList.entrySet().iterator();
+        		while (iter.hasNext()) {
+    				Entry<Integer, ArrayList<ServerInfo>> entry = iter.next();
+    				Integer key = entry.getKey();
+    				ArrayList<ServerInfo> val = entry.getValue();
+    				
+    				Logger.v(System.getProperty("line.separator"));
+    		        Logger.v("------- "+key+" size " + val.size() + " -------");
+    		        for(ServerInfo ser:val){
+    		        	Logger.v(String.format("------- %s %d %s %d ", ser.getName(),ser.getId(),ser.getHost(),ser.getPort()));
+    		        }
+    		        
+        		}
+        		
 			} catch (InvalidProtocolBufferException e) {
 				e.printStackTrace();
 			}
