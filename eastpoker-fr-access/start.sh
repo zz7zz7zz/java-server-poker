392fed4e1ad45b69c5b5879f373d75bc86fbe0c2
Long <longhui.yang@inveno.cn>
2017-12-27 19:00:16 +0800

    更新jar包

--- eastpoker-fr-access/start.sh
+++ eastpoker-fr-access/start.sh
@@ -1,29 +1,29 @@
-nohup java -jar Access.jar -n 'Access' -i 0 -p 10010 &
+nohup java -jar Access.jar -n 'Access' -i 0 -p 10010 > access_0.out &
 sleep 3
 
-nohup java -jar Access.jar -n 'Access' -i 1 -p 10011 &
+nohup java -jar Access.jar -n 'Access' -i 1 -p 10011 > access_1.out &
 sleep 3
 
-nohup java -jar Access.jar -n 'Access' -i 2 -p 10012 &
+nohup java -jar Access.jar -n 'Access' -i 2 -p 10012 > access_2.out &
 sleep 3
 
-nohup java -jar Access.jar -n 'Access' -i 3 -p 10013 &
+nohup java -jar Access.jar -n 'Access' -i 3 -p 10013 > access_3.out &
 sleep 3
 
-nohup java -jar Access.jar -n 'Access' -i 4 -p 10014 &
+nohup java -jar Access.jar -n 'Access' -i 4 -p 10014 > access_4.out &
 sleep 3
 
-nohup java -jar Access.jar -n 'Access' -i 5 -p 10015 &
+nohup java -jar Access.jar -n 'Access' -i 5 -p 10015 > access_5.out &
 sleep 3
 
-nohup java -jar Access.jar -n 'Access' -i 6 -p 10016 &
+nohup java -jar Access.jar -n 'Access' -i 6 -p 10016 > access_6.out &
 sleep 3
 
-nohup java -jar Access.jar -n 'Access' -i 7 -p 10017 &
+nohup java -jar Access.jar -n 'Access' -i 7 -p 10017 > access_7.out &
 sleep 3
 
-nohup java -jar Access.jar -n 'Access' -i 8 -p 10018 &
+nohup java -jar Access.jar -n 'Access' -i 8 -p 10018 > access_8.out &
 sleep 3
 
-nohup java -jar Access.jar -n 'Access' -i 9 -p 10019 &
+nohup java -jar Access.jar -n 'Access' -i 9 -p 10019 > access_9.out &
 sleep 3
--- eastpoker-fr-dispatcher/start.sh
+++ eastpoker-fr-dispatcher/start.sh
@@ -1,5 +1,5 @@
-nohup java -jar Dispatcher.jar -n 'Dispatcher' -i 0 -p 10000 &
+nohup java -jar Dispatcher.jar -n 'Dispatcher' -i 0 -p 10000 > dispatcher_0.out &
 sleep 3
 
-nohup java -jar Dispatcher.jar -n 'Dispatcher' -i 1 -p 10001 &
+nohup java -jar Dispatcher.jar -n 'Dispatcher' -i 1 -p 10001 > dispatcher_1.out &
 sleep 3
