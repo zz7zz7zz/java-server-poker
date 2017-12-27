acfd5181207b63eee344aee809b9909f86256def
Long <longhui.yang@inveno.cn>
2017-12-27 10:18:40 +0800

    更新配置

--- killall_server.sh
+++
@@ -1,6 +0,0 @@
-ps -ef | grep Monitor | grep -v grep | awk '{print $2}' | xargs kill -9
-
-ps -ef | grep Dispatcher | grep -v grep | awk '{print $2}' | xargs kill -9
-
-ps -ef | grep Access | grep -v grep | awk '{print $2}' | xargs kill -9
-
