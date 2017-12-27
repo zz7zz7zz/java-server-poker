3cd148d51043bea1d826c72abae20a20ed55777e
Long <longhui.yang@inveno.cn>
2017-12-27 11:59:22 +0800

    协议测试

--- eastpoker-fr-protocol/src/Main.java
+++ eastpoker-fr-protocol/src/Main.java
@@ -6,17 +6,19 @@ import com.google.protobuf.ByteString;
 import com.poker.protocols.im.ImMessageProto;
 import com.poker.protocols.server.DispatchChainProto;
 import com.poker.protocols.server.DispatchPacketProto;
+import com.poker.protocols.server.ServerInfoProto;
 
 public class Main {
 
 	public static void main(String []argc){
-		testDispatch();
+		testDispatchReg();
+//		testDispatch();
 		//testImMessage();
 	}
 	
-public static void testDispatch(){
+	public static void testDispatch(){
 		
-		// ���ն�������ݽṹ������һ��Person  
+	// 按照定义的数据结构，创建一个对象
 		DispatchPacketProto.DispatchPacket.Builder builder = DispatchPacketProto.DispatchPacket.newBuilder();
 		builder.setSequenceId(1);
 		builder.setData(ByteString.copyFrom("abc".getBytes()));
@@ -30,7 +32,7 @@ public static void testDispatch(){
 		builder.addDispatchChainList(chainBuilder);
 		
 		
-//		// ������д�����������������������������ByteArrayOutputStream������  
+		// 将数据写到输出流，如网络输出流，这里就用ByteArrayOutputStream来代替 
 		ByteArrayOutputStream output = new ByteArrayOutputStream(16*1024);
 		DispatchPacketProto.DispatchPacket obj = builder.build();
 		try {
@@ -39,15 +41,15 @@ public static void testDispatch(){
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-//		
-//		// -------------- �ָ��ߣ������Ƿ��ͷ������������л����� ---------------  
+		
+		// -------------- 分割线：上面是发送方，将数据序列化后发送 --------------- 
 		byte[] byteArray = output.toByteArray(); 
 		System.out.println(" output length " + byteArray.length + " toString " + new String(byteArray));
-//		
-//		
-//		 // -------------- �ָ��ߣ������ǽ��շ��������ݽ��պ����л� ---------------  
-//        
-//        // ���յ�������ȡ����������������������ByteArrayInputStream������  
+
+		
+		// -------------- 分割线：下面是接收方，将数据接收后反序列化 ---------------  
+        
+		// 接收到流并读取，如网络输入流，这里用ByteArrayInputStream来代替  
         ByteArrayInputStream input = new ByteArrayInputStream(byteArray);
         try {
         	DispatchPacketProto.DispatchPacket readObj = DispatchPacketProto.DispatchPacket.parseFrom(input);
@@ -59,14 +61,53 @@ public static void testDispatch(){
         
 	}
 
+	public static void testDispatchReg(){
+		
+		// 按照定义的数据结构，创建一个对象
+		ServerInfoProto.ServerInfo.Builder builder = ServerInfoProto.ServerInfo.newBuilder();
+		builder.setType(1);
+		builder.setName("name");
+		builder.setId(1);
+		builder.setHost("127.0.0.1");
+		builder.setPort(9999);
+		
+		// 将数据写到输出流，如网络输出流，这里就用ByteArrayOutputStream来代替 
+		ByteArrayOutputStream output = new ByteArrayOutputStream(16*1024);
+		ServerInfoProto.ServerInfo obj = builder.build();
+		try {
+			obj.writeTo(output);
+			//op.write(obj.toByteArray())
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// -------------- 分割线：上面是发送方，将数据序列化后发送 ---------------
+		byte[] byteArray = output.toByteArray(); 
+		System.out.println(" output length " + byteArray.length + " toString " + new String(byteArray));
+		
+		
+		// -------------- 分割线：下面是接收方，将数据接收后反序列化 ---------------  
+        
+		// 接收到流并读取，如网络输入流，这里用ByteArrayInputStream来代替  
+        ByteArrayInputStream input = new ByteArrayInputStream(byteArray);
+        try {
+        	ServerInfoProto.ServerInfo readObj = ServerInfoProto.ServerInfo.parseFrom(input);
+			
+			System.out.println(" input length " + byteArray.length + " toString " + readObj.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        
+	}
+	
 	public static void testImMessage(){
 		
-		// ���ն�������ݽṹ������һ��Person  
+		// 按照定义的数据结构，创建一个对象
 		ImMessageProto.ImMessage.Builder builder = ImMessageProto.ImMessage.newBuilder();
 		builder.setType(1);
 		builder.setText("Im123456789");
 		
-		// ������д�����������������������������ByteArrayOutputStream������  
+		// 将数据写到输出流，如网络输出流，这里就用ByteArrayOutputStream来代替 
 		ByteArrayOutputStream output = new ByteArrayOutputStream(16*1024);
 		ImMessageProto.ImMessage obj = builder.build();
 		try {
@@ -76,14 +117,14 @@ public static void testDispatch(){
 			e.printStackTrace();
 		}
 		
-		// -------------- �ָ��ߣ������Ƿ��ͷ������������л����� ---------------  
+		// -------------- 分割线：上面是发送方，将数据序列化后发送 ---------------
 		byte[] byteArray = output.toByteArray(); 
 		System.out.println(" output length " + byteArray.length + " toString " + new String(byteArray));
 		
 		
-		 // -------------- �ָ��ߣ������ǽ��շ��������ݽ��պ����л� ---------------  
+		// -------------- 分割线：下面是接收方，将数据接收后反序列化 ---------------  
         
-        // ���յ�������ȡ����������������������ByteArrayInputStream������  
+		// 接收到流并读取，如网络输入流，这里用ByteArrayInputStream来代替  
         ByteArrayInputStream input = new ByteArrayInputStream(byteArray);
         try {
         	ImMessageProto.ImMessage readObj = ImMessageProto.ImMessage.parseFrom(input);
