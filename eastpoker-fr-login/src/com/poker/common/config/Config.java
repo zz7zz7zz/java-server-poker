8b7d3b8e1998407cd96291af7b1a05ae51873068
Long <longhui.yang@inveno.cn>
2017-12-27 21:14:33 +0800

    登录服务

---
+++ eastpoker-fr-login/src/com/poker/common/config/Config.java
@@ -0,0 +1,61 @@
+package com.poker.common.config;
+
+import java.util.HashMap;
+
+import com.open.net.client.structures.TcpAddress;
+import com.open.net.client.structures.UdpAddress;
+import com.open.net.server.utils.CfgParser;
+
+public class Config {
+
+	//Dispatcher
+    public TcpAddress[] dispatcher_net_tcp;
+    public UdpAddress[] dispatcher_net_udp;
+    
+    //Monitor
+    public UdpAddress[] monitor_net_udp;
+    
+    //解析文件配置参数
+    public final void initFileConfig(String config_path) {
+        HashMap<String,Object> map = CfgParser.parseToMap(config_path);
+        initFileConfig(map);
+    }
+    
+    //-------------------------------------------------------------------------------------------
+    protected void initFileConfig(HashMap<String,Object> map){
+    	if(null !=map){
+            String val[]     = CfgParser.getStringArray(map,"Dispatcher","net_tcp");
+            if(null != val){
+            	dispatcher_net_tcp = new TcpAddress[val.length];
+                for (int i = 0; i < val.length; i++) {
+                    String[] v = val[i].split(":");
+                    if(v.length>1){
+                    	dispatcher_net_tcp[i]  = new TcpAddress(v[0],Integer.valueOf(v[1]));
+                    }
+                }
+            }
+            
+            val              = CfgParser.getStringArray(map,"Dispatcher","net_udp");
+            if(null != val){
+            	dispatcher_net_udp = new UdpAddress[val.length];
+                for (int i = 0; i < val.length; i++) {
+                    String[] v = val[i].split(":");
+                    if(v.length>1){
+                    	dispatcher_net_udp[i] = new UdpAddress(v[0],Integer.valueOf(v[1]));
+                    }
+                }
+            }
+            
+            val              = CfgParser.getStringArray(map,"Monitor","net_udp");
+            if(null != val){
+            	monitor_net_udp = new UdpAddress[val.length];
+                for (int i = 0; i < val.length; i++) {
+                    String[] v = val[i].split(":");
+                    if(v.length>1){
+                    	monitor_net_udp[i] = new UdpAddress(v[0],Integer.valueOf(v[1]));
+                    }
+                }
+            }
+       }
+    }
+}
---
+++ eastpoker-fr-login/src/com/poker/login/Main.java
@@ -0,0 +1,204 @@
+package com.poker.login;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import com.open.net.client.impl.tcp.nio.NioClient;
+import com.open.net.client.structures.BaseClient;
+import com.open.net.client.structures.BaseMessageProcessor;
+import com.open.net.client.structures.IConnectListener;
+import com.open.net.client.structures.TcpAddress;
+import com.open.net.server.GServer;
+import com.open.net.server.impl.tcp.nio.NioServer;
+import com.open.net.server.structures.AbstractClient;
+import com.open.net.server.structures.AbstractMessageProcessor;
+import com.open.net.server.structures.ServerConfig;
+import com.open.net.server.structures.ServerLog;
+import com.open.net.server.structures.ServerLog.LogListener;
+import com.open.net.server.structures.message.Message;
+import com.open.net.server.utils.NetUtil;
+import com.open.util.log.Logger;
+import com.poker.base.Server;
+import com.poker.common.config.Config;
+import com.poker.data.DataPacket;
+import com.poker.protocols.DataTransfer;
+
+/**
+ * author       :   long
+ * created on   :   2017/11/30
+ * description  :  服务器入口
+ */
+
+public class Main {
+
+    public static void main(String [] args){
+    	
+        //-------------------------------------------------------------------------------------------
+    	//1.1 配置初始化
+        ServerConfig mServerInfo = new ServerConfig();
+        mServerInfo.initArgsConfig(args);
+        mServerInfo.initFileConfig("./conf/lib.server.config");
+        
+        //1.2 数据初始化
+        GServer.init(mServerInfo, com.open.net.server.impl.tcp.nio.NioClient.class);
+        
+        //1.3 日志初始化
+        Logger.init("./conf/lib.log.config",mServerInfo.id);
+        Logger.addFilterTraceElement(ServerLog.class.getName());
+        Logger.addFilterTraceElement(mLogListener.getClass().getName());
+        Logger.v("-------Server------"+ mServerInfo.toString());
+        
+        //1.4 业务配置初始化
+        Config mConfig = new Config();
+        mConfig.initFileConfig("./conf/server.config");
+        
+        //-------------------------------------------------------------------------------------------
+        //2.1 发送给服务监听器
+        register_monitor(mConfig);
+    	
+    	//2.2 连接到Dispatcher
+    	register_dispatcher(mConfig);
+    	
+        //-------------------------------------------------------------------------------------------
+        //3.0 连接初始化
+        Logger.v("-------Server------start---------");
+        try {
+            NioServer mNioServer = new NioServer(mServerInfo,mMessageProcessor,mLogListener);
+            mNioServer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } 
+        Logger.v("-------Server------end---------");
+        
+        //-------------------------------------------------------------------------------------------
+        //4.0 连接初始化
+        unregister_dispatcher(mConfig);
+        unregister_monitor(mConfig);
+    }
+
+    //---------------------------------------Monitor----------------------------------------------------
+    public static void register_monitor(Config mConfig){
+        byte[] buff = DataTransfer.register2Monitor(Server.SERVER_LOGIN,GServer.mServerInfo.name, GServer.mServerInfo.id,GServer.mServerInfo.host, GServer.mServerInfo.port);
+        int monitorSize = (null != mConfig.monitor_net_udp) ? mConfig.monitor_net_udp.length : 0;
+    	if(monitorSize > 0){
+    		for(int i=0; i< monitorSize ; i++){
+    			NetUtil.send_data_by_udp_nio(mConfig.monitor_net_udp[i].ip, mConfig.monitor_net_udp[i].port,buff,0,DataPacket.Header.getLength(buff));
+    		}
+    	}
+    }
+    
+    public static void unregister_monitor(Config mConfig){
+        byte[] buff = DataTransfer.unregister2Monitor(Server.SERVER_LOGIN,GServer.mServerInfo.name, GServer.mServerInfo.id,GServer.mServerInfo.host, GServer.mServerInfo.port);
+        int monitorSize = (null != mConfig.monitor_net_udp) ? mConfig.monitor_net_udp.length : 0;
+    	if(monitorSize > 0){
+    		for(int i=0; i< monitorSize ; i++){
+    			NetUtil.send_data_by_udp_nio(mConfig.monitor_net_udp[i].ip, mConfig.monitor_net_udp[i].port,buff,0,DataPacket.Header.getLength(buff));
+    		}
+    	}
+    }
+    
+    //---------------------------------------Dispatcher----------------------------------------------------
+    public static void register_dispatcher(Config mConfig){
+    	int dispatcherSize = (null != mConfig.dispatcher_net_tcp) ? mConfig.dispatcher_net_tcp.length : 0;
+    	if(dispatcherSize > 0){
+    		dispatcher = new NioClient[dispatcherSize];
+    		for(int i=0; i< dispatcherSize ; i++){
+    			dispatcher[i] = new NioClient(mDisPatcherMessageProcessor,mDisPatcherConnectResultListener); 
+    			dispatcher[i].setConnectAddress(new TcpAddress[]{mConfig.dispatcher_net_tcp[i]});
+    			dispatcher[i].connect();
+    		}
+    	}
+    }
+    
+    public static void unregister_dispatcher(Config mConfig){
+    	int dispatcherSize = (null != dispatcher) ? dispatcher.length : 0;
+    	if(dispatcherSize > 0){
+    		for(int i=0; i< dispatcherSize ; i++){
+    			dispatcher[i].disconnect();
+    		}
+    	}
+    }
+    
+    public static NioClient [] dispatcher;
+    
+	private static IConnectListener mDisPatcherConnectResultListener = new IConnectListener() {
+		@Override
+		public void onConnectionSuccess(BaseClient client) {
+			Logger.v("-------dispatcher onConnectionSuccess---------" +Arrays.toString(((NioClient)client).getConnectAddress()));
+			//register to dispatchServer
+			byte[] buff = DataTransfer.register2Dispatcher(Server.SERVER_LOGIN,GServer.mServerInfo.name, GServer.mServerInfo.id,GServer.mServerInfo.host, GServer.mServerInfo.port);
+			mDisPatcherMessageProcessor.send(client,buff,0,DataPacket.Header.getLength(buff));
+		}
+
+		@Override
+		public void onConnectionFailed(BaseClient client) {
+
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			for(NioClient mClient: dispatcher){
+				if(mClient == client){
+					Logger.v("-------dispatcher onConnectionFailed---------" +Arrays.toString(((NioClient)client).getConnectAddress()));
+					mClient.connect();
+					break;
+				}
+			}
+		}
+	};
+
+	private static BaseMessageProcessor mDisPatcherMessageProcessor =new BaseMessageProcessor() {
+
+		@Override
+		public void onReceiveMessages(BaseClient arg0,
+				LinkedList<com.open.net.client.structures.message.Message> arg1) {
+			// TODO Auto-generated method stub
+			
+		}
+	};
+	
+    //-------------------------------------------------------------------------------------------
+    public static AbstractMessageProcessor mMessageProcessor = new AbstractMessageProcessor() {
+
+    	private ByteBuffer mWriteBuffer  = ByteBuffer.allocate(16*1024);
+        private long oldTime = System.currentTimeMillis();
+        private long nowTime  = oldTime;
+        
+        protected void onReceiveMessage(AbstractClient client, Message msg){
+
+        	Logger.v("onReceiveMessage 0x" + Integer.toHexString(DataPacket.Header.getCmd(msg.data, msg.offset)));
+        	
+        }
+        
+		@Override
+		public void onTimeTick() {
+			nowTime = System.currentTimeMillis();
+			if(nowTime - oldTime > 1000){
+				oldTime = nowTime;
+				
+			}
+		}
+
+		@Override
+		public void onClientEnter(AbstractClient client) {
+			Logger.v("onClientEnter " + client.mClientId);
+		}
+
+		@Override
+		public void onClientExit(AbstractClient client) {
+			Logger.v("onClientExit " + client.mClientId);
+		}
+    };
+    
+    public static LogListener mLogListener = new LogListener(){
+
+		@Override
+		public void onLog(String tag, String msg) {
+			Logger.v(msg);
+		}
+    };
+}
