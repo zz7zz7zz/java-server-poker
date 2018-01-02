e5709c61b1123275035fb6b064362b0478cfeaa8
Long <longhui.yang@inveno.cn>
2018-01-02 18:58:23 +0800

    更新jar包

--- eastpoker-fr-monitor/src/com/poker/monitor/Main.java
+++ eastpoker-fr-monitor/src/com/poker/monitor/Main.java
@@ -3,10 +3,8 @@ package com.poker.monitor;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
 import java.util.Map.Entry;
 
 import com.google.protobuf.InvalidProtocolBufferException;
@@ -21,6 +19,7 @@ import com.open.net.server.structures.ServerLog.LogListener;
 import com.open.net.server.structures.message.Message;
 import com.open.net.server.utils.TextUtils;
 import com.open.util.log.Logger;
+import com.poker.cmd.MonitorCmd;
 import com.poker.data.DataPacket;
 import com.poker.protocols.server.ServerInfoProto;
 import com.poker.protocols.server.ServerInfoProto.ServerInfo;;
@@ -70,57 +69,44 @@ public class Main {
         
         protected void onReceiveMessage(AbstractClient client, Message msg){
         	try {
-        		Logger.v("onReceiveMessage 0x" + Integer.toHexString(DataPacket.Header.getCmd(msg.data, msg.offset)));
         		
-        		ServerInfo enterServer = ServerInfo.parseFrom(msg.data,DataPacket.Header.HEADER_LENGTH+msg.offset,msg.length-DataPacket.Header.HEADER_LENGTH);
-        		ArrayList<ServerInfo> serverArray = serverOnlineList.get(enterServer.getType());
-        		if(null == serverArray){
-        			serverArray = new ArrayList<ServerInfo>(10);
-        			serverOnlineList.put(enterServer.getType(), serverArray);
-        		}else{
-            		for(ServerInfo obj:serverArray){
-            			if(obj.getId() == enterServer.getId()){
-            				serverArray.remove(obj);
-            				break;
-            			}
+        		int cmd = DataPacket.getCmd(msg.data, msg.offset);
+        		Logger.v("onReceiveMessage 0x" + Integer.toHexString(cmd));
+        		
+        		if(cmd == MonitorCmd.CMD_REGISTER){
+            		ServerInfo enterServer = ServerInfo.parseFrom(msg.data,DataPacket.Header.HEADER_LENGTH+msg.offset,msg.length-DataPacket.Header.HEADER_LENGTH);
+            		ArrayList<ServerInfo> serverArray = serverOnlineList.get(enterServer.getType());
+            		if(null == serverArray){
+            			serverArray = new ArrayList<ServerInfo>(10);
+            			serverOnlineList.put(enterServer.getType(), serverArray);
+            		}else{
+                		for(ServerInfo obj:serverArray){
+                			if(obj.getId() == enterServer.getId()){
+                				serverArray.remove(obj);
+                				break;
+                			}
+                		}
+            		}
+            		serverArray.add(enterServer);
+
+            		//打印所有的服务
+            		Iterator<Entry<Integer, ArrayList<ServerInfo>>> iter = serverOnlineList.entrySet().iterator();
+            		while (iter.hasNext()) {
+        				Entry<Integer, ArrayList<ServerInfo>> entry = iter.next();
+        				Integer key = entry.getKey();
+        				ArrayList<ServerInfo> val = entry.getValue();
+        				
+        				Logger.v(System.getProperty("line.separator"));
+        		        Logger.v("------- "+key+" size " + val.size() + " -------");
+        		        for(ServerInfo ser:val){
+        		        	Logger.v(String.format("------- name %s id %d host %s port %d ", ser.getName(),ser.getId(),!TextUtils.isEmpty(ser.getHost())? ser.getHost() : "null",ser.getPort()));
+        		        }
+        		        
             		}
         		}
-        		serverArray.add(enterServer);
-
-        		//打印所有的服务
-        		Iterator<Entry<Integer, ArrayList<ServerInfo>>> iter = serverOnlineList.entrySet().iterator();
-        		while (iter.hasNext()) {
-    				Entry<Integer, ArrayList<ServerInfo>> entry = iter.next();
-    				Integer key = entry.getKey();
-    				ArrayList<ServerInfo> val = entry.getValue();
-    				
-    				Logger.v(System.getProperty("line.separator"));
-    		        Logger.v("------- "+key+" size " + val.size() + " -------");
-    		        for(ServerInfo ser:val){
-    		        	Logger.v(String.format("------- name %s id %d host %s port %d ", ser.getName(),ser.getId(),!TextUtils.isEmpty(ser.getHost())? ser.getHost() : "null",ser.getPort()));
-    		        }
-    		        
-        		}
-        		
 			} catch (InvalidProtocolBufferException e) {
 				e.printStackTrace();
 			}
-//            Logger.v("onReceiveMessage()  "+new String(msg.data,msg.offset,msg.length));
-//            String data ="MainUdpBioServer--onReceiveMessage()--src_reuse_type "+msg.src_reuse_type
-//                    + " dst_reuse_type " + msg.dst_reuse_type
-//                    + " block_index " +msg.block_index
-//                    + " offset " +msg.offset;
-//            Logger.v("--onReceiveMessage()--reply "+data);
-//            
-//            byte[] response = data.getBytes();
-//
-//
-//            mWriteBuffer.clear();
-//            mWriteBuffer.put(response,0,response.length);
-//            mWriteBuffer.flip();
-////        unicast(client,mWriteBuffer.array(),0,response.length);
-//            broadcast(mWriteBuffer.array(),0,response.length);
-//            mWriteBuffer.clear();
         }
 
 		@Override
