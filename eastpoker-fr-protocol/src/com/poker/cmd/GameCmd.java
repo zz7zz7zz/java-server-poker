98317948718f392536490f3f3fef622865ebd425
Long <longhui.yang@inveno.cn>
2018-01-05 19:26:49 +0800

    定义基础命令字

--- eastpoker-fr-protocol/src/com/poker/cmd/GameCmd.java
+++ eastpoker-fr-protocol/src/com/poker/cmd/GameCmd.java
@@ -1,5 +1,28 @@
 package com.poker.cmd;
 
 public final class GameCmd {
-
+	
+	//-----------------------------------------------------
+	public int CMD_USER_ENTER 				= 0x1;//用户进入
+	public int CMD_BROADCAST_USER_ENTER  	= 0x2;
+	
+	public int CMD_USER_EXIT             	= 0x3;//用户退出
+	public int CMD_BROADCAST_USER_EXIT   	= 0x4;
+	
+	public int CMD_USER_READY 				= 0x5;//用户准备
+	public int CMD_BROADCAST_USER_READY  	= 0x6;
+	
+	public int CMD_USER_OFFLINE     		= 0x7;//用户掉线
+	public int CMD_BROADCAST_USER_OFFLINE 	= 0x8;
+	
+	//-----------------------------------------------------
+	public int CMD_KICK_USER 	    		= 0x9;//用户踢人
+	public int CMD_BROADCAST_KICK_USER		= 0xA;
+	
+	public int CMD_ONLOOKERS     		    = 0xB;//用户申请围观
+	public int CMD_BROADCAST_ONLOOKERS 	    = 0xC;
+	
+	//-----------------------------------------------------
+	
+	
 }
