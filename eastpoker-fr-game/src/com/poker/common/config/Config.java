66ad887f7c37ad02ff7a89e7774edb9aa51e62a6
long <zz7zz7zz.zz@hotmail.com>
2018-01-07 23:45:51 +0800

    更新配置项

--- eastpoker-fr-game/src/com/poker/common/config/Config.java
+++ eastpoker-fr-game/src/com/poker/common/config/Config.java
@@ -9,9 +9,6 @@ import com.open.net.server.utils.CfgParser;
 
 public class Config {
 
-	//Allocator
-	public int game_id;
-	
 	//Dispatcher
     public TcpAddress[] dispatcher_net_tcp;
     public UdpAddress[] dispatcher_net_udp;
@@ -19,6 +16,11 @@ public class Config {
     //Monitor
     public UdpAddress[] monitor_net_udp;
     
+	//Allocator
+    public int server_id;
+	public int game_id;
+	public int game_level;
+	
     //桌子信息
     public int table_count = 0;                   //一个房间中对桌子数量
 
@@ -45,13 +47,13 @@ public class Config {
     		
     		game_id = CfgParser.getInt(map, "Game","game");
     		
-    		table_count = CfgParser.getInt(map, "Room","table_count");
-    		table_min_user = CfgParser.getInt(map, "Room","table_min_user");
-    		table_max_user = CfgParser.getInt(map, "Room","table_max_user");
-    		table_init_chip = CfgParser.getInt(map, "Room","table_init_chip");
-    		table_init_ante = CfgParser.getInt(map, "Room","table_init_ante");
+    		table_count = CfgParser.getInt(map, "Game","table_count");
+    		table_min_user = CfgParser.getInt(map, "Game","table_min_user");
+    		table_max_user = CfgParser.getInt(map, "Game","table_max_user");
+    		table_init_chip = CfgParser.getInt(map, "Game","table_init_chip");
+    		table_init_ante = CfgParser.getInt(map, "Game","table_init_ante");
     		
-        String val[]     = CfgParser.getStringArray(map,"Room","table_bilnd");
+        String val[]     = CfgParser.getStringArray(map,"Game","table_bilnd");
         if(null != val){
         		table_bilnd = new int[val.length];
             for (int i = 0; i < val.length; i++) {
@@ -59,7 +61,7 @@ public class Config {
             }
         }
             
-        val    = CfgParser.getStringArray(map,"Room","table_bilnd_time");
+        val    = CfgParser.getStringArray(map,"Game","table_bilnd_time");
         if(null != val){
         		table_bilnd_time = new int[val.length];
             for (int i = 0; i < val.length; i++) {
@@ -67,7 +69,7 @@ public class Config {
             }
         }
         
-    		table_action_timeout = CfgParser.getInt(map, "Room","table_action_timeout");
+    		table_action_timeout = CfgParser.getInt(map, "Game","table_action_timeout");
     		
             val     = CfgParser.getStringArray(map,"Dispatcher","net_tcp");
             if(null != val){
@@ -106,14 +108,14 @@ public class Config {
 
 	@Override
 	public String toString() {
-		return "Config [game_id=" + game_id + ", dispatcher_net_tcp=" + Arrays.toString(dispatcher_net_tcp)
-				+ ", dispatcher_net_udp=" + Arrays.toString(dispatcher_net_udp) + ", monitor_net_udp="
-				+ Arrays.toString(monitor_net_udp) + ", table_count=" + table_count + ", table_min_user="
-				+ table_min_user + ", table_max_user=" + table_max_user + ", table_init_chip=" + table_init_chip
-				+ ", table_bilnd=" + Arrays.toString(table_bilnd) + ", table_bilnd_time="
-				+ Arrays.toString(table_bilnd_time) + ", table_action_timeout=" + table_action_timeout + "]";
+		return "Config [dispatcher_net_tcp=" + Arrays.toString(dispatcher_net_tcp) + ", dispatcher_net_udp="
+				+ Arrays.toString(dispatcher_net_udp) + ", monitor_net_udp=" + Arrays.toString(monitor_net_udp)
+				+ ", game_id=" + game_id + ", game_level=" + game_level + ", table_count=" + table_count
+				+ ", table_min_user=" + table_min_user + ", table_max_user=" + table_max_user + ", table_min_chip="
+				+ table_min_chip + ", table_max_chip=" + table_max_chip + ", table_init_chip=" + table_init_chip
+				+ ", table_init_ante=" + table_init_ante + ", table_bilnd=" + Arrays.toString(table_bilnd)
+				+ ", table_bilnd_time=" + Arrays.toString(table_bilnd_time) + ", table_action_timeout="
+				+ table_action_timeout + "]";
 	}
-
-    
     
 }
