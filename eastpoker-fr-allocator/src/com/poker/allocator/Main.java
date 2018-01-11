3c84a87c6be4d586094d50a75768127d67ae116f
Long <longhui.yang@inveno.cn>
2018-01-11 20:40:22 +0800

    消息粘包处理

--- eastpoker-fr-allocator/src/com/poker/allocator/Main.java
+++ eastpoker-fr-allocator/src/com/poker/allocator/Main.java
@@ -3,6 +3,7 @@ package com.poker.allocator;
 import java.util.Arrays;
 import java.util.LinkedList;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import com.open.net.client.GClient;
 import com.open.net.client.impl.tcp.nio.NioClient;
 import com.open.net.client.message.Message;
@@ -11,6 +12,7 @@ import com.open.net.client.object.AbstractClientMessageProcessor;
 import com.open.net.client.object.ClientConfig;
 import com.open.net.client.object.IConnectListener;
 import com.open.net.client.object.TcpAddress;
+import com.open.net.client.message.MessageBuffer;
 import com.open.net.server.object.ArgsConfig;
 import com.open.net.server.object.ServerConfig;
 import com.open.net.server.object.ServerLog;
@@ -177,28 +179,149 @@ public class Main {
 		public void onReceiveMessages(AbstractClient mClient, LinkedList<Message> list) {
 			for(int i = 0 ;i<list.size();i++){
 				Message msg = list.get(i);
+				onReceiveMessage(mClient,msg);
+			}
+		}
+		
+		 protected void onReceiveMessage(AbstractClient client, Message msg){
 
-	        	try{
-	        		
-					int cmd = DataPacket.getCmd(msg.data, msg.offset);
-					int squenceId = DataPacket.getSequenceId(msg.data,msg.offset);
-					
-		        	String sCmd = Integer.toHexString(cmd);
-
-		        	System.out.println(String.format("onReceiveMessage mDisPatcherMessageProcessor 0x%s  squenceId %s",sCmd,squenceId));
-		        	Logger.v(String.format("onReceiveMessage mDisPatcherMessageProcessor 0x%s  squenceId %s",sCmd,squenceId));
-		        	
-		        	if(cmd == AllocatorCmd.CMD_REPORT_ROOMINFO){
-		        		mHandler.on_report_roominfo(mClient,msg);
-		        	}else if(cmd == AllocatorCmd.CMD_GET_ROOMINFO){
-		        		mHandler.on_get_roominfo(mClient,msg);
-		        	}else if(cmd == AllocatorCmd.CMD_UPDATE_ROOMINFO){
-		        		mHandler.on_update_roominfo(mClient,msg);
-		        	}
-	        	}catch(Exception e){
-	        		e.printStackTrace();
+	        	//过滤异常Message
+	        	if(null == client || msg.length<=0){
+	        		return;
 	        	}
+	        	
+	    		//对数据进行拆包/组包过程
+	        	int code = 0;
+	        	int full_packet_count = 0;
+	        	int half_packet_count = 0;
+	        	//packet struct like this : |--head--|-----body-----|
+	        	
+	        	if(null == client.mReceivingMsg){
+					int msg_offset = 0;
+					int header_length = DataPacket.getHeaderLength();
+					while(true){
+						int the_rest_msg_length = msg.length -msg_offset ;
+						if(the_rest_msg_length == 0){
+							code = 1;
+							break;
+						}else if(the_rest_msg_length <= header_length){//说明还没有接收完完整的一个包头，继续读取
+		    				client.mReceivingMsg = new Message();
+		    				System.arraycopy(msg.data,msg.offset+msg_offset,client.mReceivingMsg.data,client.mReceivingMsg.offset,the_rest_msg_length);
+	        				client.mReceivingMsg.length = the_rest_msg_length;
+	        				
+	        				code = -101;//不足包头
+	        				half_packet_count++;
+	        				break;
+		    			}else if(the_rest_msg_length > header_length){
+	            			int header_start 	= msg.offset+ msg_offset;
+		    				int packetLength = DataPacket.getLength(msg.data,header_start);
+		            		if(the_rest_msg_length >= packetLength){//说明可以凑成一个包
 
+		            			int body_start 		= header_start + header_length;
+		            			int body_length     = DataPacket.getLength(msg.data, header_start)-header_length;
+		            			
+		            			int cmd = DataPacket.getCmd(msg.data, header_start);
+		            			onHandleCmd(client,cmd,msg,body_start,body_length);
+		            			msg_offset += packetLength;
+		            			
+		            			full_packet_count++;
+		            			continue;
+		            		}else{//如果不足一个包
+		            			int capacity = 16384;//16KB
+		            			if(the_rest_msg_length >= DataPacket.Header.OFFSET_SEQUENCEID){//可以读出包体的长度,尽量传递真实的长度
+		            				capacity = DataPacket.getLength(msg.data,msg.offset+msg_offset);
+		            			}
+			    				client.mReceivingMsg = MessageBuffer.getInstance().buildWithCapacity(capacity,msg.data,msg.offset+msg_offset,the_rest_msg_length);
+		        				
+		        				code = -102;//足包头，不足包体-->不足整包
+		        				half_packet_count++;
+		        				break;
+		            		}
+		    			}
+					}
+	        	}else{//说明有分包现象，只接收了部分包，未收到整包
+					int msg_offset = 0;
+					int header_length = DataPacket.getHeaderLength();
+	    			while(true){
+	    				int the_rest_msglength = msg.length -msg_offset ;
+	    				if(the_rest_msglength == 0){
+	    					code = 2;
+	    					break;
+	    				}
+	    				
+	    				if(client.mReceivingMsg.length < header_length){//说明还没有读取完完整的一个包头，继续读取
+	        				int remain_header_length = header_length - client.mReceivingMsg.length;
+	        				int real_read_remain_header_length = Math.min(remain_header_length, the_rest_msglength);
+	        				if(real_read_remain_header_length >0){
+	            				System.arraycopy(msg.data,msg.offset+msg_offset,client.mReceivingMsg.data,client.mReceivingMsg.offset+client.mReceivingMsg.length,real_read_remain_header_length);
+	            				client.mReceivingMsg.length += real_read_remain_header_length;
+	            				msg_offset += real_read_remain_header_length;
+	        				}
+	        			}
+	        			
+	        			if(client.mReceivingMsg.length >= header_length){//说明包头读完了，接着读取包体了
+	        				int packetLength = DataPacket.getLength(client.mReceivingMsg.data,client.mReceivingMsg.offset);
+	        				int remain_packet_length = packetLength - client.mReceivingMsg.length;
+	        				int real_read_remain_packet_length = Math.min(remain_packet_length, the_rest_msglength);
+	        				if(real_read_remain_packet_length >0){
+	            				System.arraycopy(msg.data,msg.offset+msg_offset,client.mReceivingMsg.data,client.mReceivingMsg.offset+client.mReceivingMsg.length,real_read_remain_packet_length);
+	            				client.mReceivingMsg.length += real_read_remain_packet_length;
+	            				msg_offset += real_read_remain_packet_length;
+	        				}
+	        				
+	        				if(client.mReceivingMsg.length == packetLength){//说明包完整了
+	        					
+		            			int header_start 	= client.mReceivingMsg.offset;
+		            			int body_start 		= header_start + header_length;
+		            			int body_length     = DataPacket.getLength(client.mReceivingMsg.data, header_start)-header_length;
+		            			
+	        					int cmd = DataPacket.getCmd(client.mReceivingMsg.data, header_start);
+		            			onHandleCmd(client,cmd,client.mReceivingMsg,body_start,body_length);
+		            			
+	        					full_packet_count++;
+	        					
+	        					client.mReceivingMsg.length = 0;
+	                			if(msg.length - msg_offset > 0){//说明需要继续接收,还有粘包现象
+	                				continue;
+	                			}else{//说明可以回收
+	                				code = 3;
+	                				MessageBuffer.getInstance().release(client.mReceivingMsg);
+	                				client.mReceivingMsg = null;
+	                				break;
+	                			}
+	        				}else if(client.mReceivingMsg.length < packetLength){//说明包还未完整
+	            				code = -202;
+	            				half_packet_count++;
+	        					break;
+	        				}else {//说明异常了，需要重连
+	        					code = -203;
+	        					break;
+	        				}
+	        			}else{
+	        				code = -201;//不足包头
+	        				half_packet_count++;
+	        				break;
+	        			}
+	    			}
+	    		}
+	    		
+	    		Logger.v("code "+ code +" full_packet_count " + full_packet_count + " half_packet_count " + half_packet_count + System.getProperty("line.separator"));
+	        }
+		 
+		public void onHandleCmd(AbstractClient client, int cmd ,Message msg,int body_start,int body_length){
+        	try {
+        		
+        		Logger.v("onReceiveMessage 0x" + Integer.toHexString(cmd));
+        		
+	        	if(cmd == AllocatorCmd.CMD_REPORT_ROOMINFO){
+	        		mHandler.on_report_roominfo(client,msg);
+	        	}else if(cmd == AllocatorCmd.CMD_GET_ROOMINFO){
+	        		mHandler.on_get_roominfo(client,msg);
+	        	}else if(cmd == AllocatorCmd.CMD_UPDATE_ROOMINFO){
+	        		mHandler.on_update_roominfo(client,msg);
+	        	}
+			} catch (InvalidProtocolBufferException e) {
+				e.printStackTrace();
 			}
 		}
 	};
