4993ca02307933c0478b186864c6ae3f7be0178e
Long <longhui.yang@inveno.cn>
2018-01-15 20:08:17 +0800

    更新扩展协议

--- eastpoker-fr-login/src/com/poker/login/handler/ClientMessageProcessor.java
+++ eastpoker-fr-login/src/com/poker/login/handler/ClientMessageProcessor.java
@@ -44,110 +44,200 @@ public class ClientMessageProcessor extends AbstractClientMessageProcessor {
     	//packet struct like this : |--head--|-----body-----|
     	
     	if(null == client.mReceivingMsg){
+    		
 			int msg_offset = 0;
-			int header_length = DataPacket.getHeaderLength();
+			int msg_length = msg.length ;
+			int base_header_length = DataPacket.getBaseHeaderLength();
+			
 			while(true){
-				int the_rest_msg_length = msg.length -msg_offset ;
-				if(the_rest_msg_length == 0){
-					code = 1;
+				
+				if(msg_length == 0){
+					code = 10;
 					break;
-				}else if(the_rest_msg_length <= header_length){//说明还没有接收完完整的一个包头，继续读取
-        			int capacity = 16384;//16KB
-        			if(the_rest_msg_length >= DataPacket.Header.OFFSET_SEQUENCEID){//可以读出包体的长度,尽量传递真实的长度
-        				capacity = DataPacket.getLength(msg.data,msg.offset+msg_offset);
+				}
+				
+				//1.说明不足基本包头，继续读取
+    			int header_start  = msg.offset+ msg_offset;
+				if(msg_length <= base_header_length){
+        			int next_pkg_length = 16384;//16KB
+        			if(msg_length >= DataPacket.Header.HEADER_OFFSET_SEQUENCEID){//可以读出包体的长度,尽量传递真实的长度
+        				next_pkg_length = DataPacket.getLength(msg.data,header_start);
         			}
-    				client.mReceivingMsg = MessageBuffer.getInstance().buildWithCapacity(capacity,msg.data,msg.offset+msg_offset,the_rest_msg_length);
-    				
-    				code = -101;//不足包头
+    				client.mReceivingMsg = MessageBuffer.getInstance().buildWithCapacity(next_pkg_length,msg.data,msg.offset+msg_offset,msg_length);
+
+    				code = -101;//不足基本包头
     				half_packet_count++;
     				break;
-    			}else if(the_rest_msg_length > header_length){
-        			int header_start 	= msg.offset+ msg_offset;
-    				int packetLength = DataPacket.getLength(msg.data,header_start);
-            		if(the_rest_msg_length >= packetLength){//说明可以凑成一个包
+    			}
+				
+				//2.说明足基本包头，不足扩展包头
+				int header_length = DataPacket.getHeaderLength(msg.data, header_start);
+				if(msg_length <= header_length){
+					int next_pkg_length = 16384;//16KB
+        			if(msg_length >= DataPacket.Header.HEADER_OFFSET_SEQUENCEID){//可以读出包体的长度,尽量传递真实的长度
+        				next_pkg_length = DataPacket.getLength(msg.data,header_start);
+        			}
+    				client.mReceivingMsg = MessageBuffer.getInstance().buildWithCapacity(next_pkg_length,msg.data,msg.offset+msg_offset,msg_length);
 
-            			int body_start 		= header_start + header_length;
-            			int body_length     = DataPacket.getLength(msg.data, header_start)-header_length;
+    				code = -102;//不足扩展包头
+    				half_packet_count++;
+    				break;
+				}
+				
+				//3.说明包头,扩展包头读完了，接着读取包体
+				if(msg_length > header_length){
+    				int packet_length = DataPacket.getLength(msg.data,header_start);
+            		if(msg_length >= packet_length){//说明可以凑成一个包
             			
-            			int cmd = DataPacket.getCmd(msg.data, header_start);
-            			dispatchMessage(client,cmd,msg,header_start,header_length,body_start,body_length);
-            			msg_offset += packetLength;
+            			int body_start 		= header_start  + header_length;
+            			int body_length     = packet_length - header_length;
+            			
+            			dispatchMessage(client,msg,header_start,header_length,body_start,body_length);
+            			
+            			msg_offset += packet_length;
+            			msg_length -= packet_length;
             			
             			full_packet_count++;
             			continue;
-            		}else{//如果不足一个包
-            			int capacity = 16384;//16KB
-            			if(the_rest_msg_length >= DataPacket.Header.OFFSET_SEQUENCEID){//可以读出包体的长度,尽量传递真实的长度
-            				capacity = DataPacket.getLength(msg.data,msg.offset+msg_offset);
-            			}
-	    				client.mReceivingMsg = MessageBuffer.getInstance().buildWithCapacity(capacity,msg.data,msg.offset+msg_offset,the_rest_msg_length);
+            		}else{//如果不足一个包(足包头，不足包体)
+            			int next_pkg_length = packet_length;
+	    				client.mReceivingMsg = MessageBuffer.getInstance().buildWithCapacity(next_pkg_length,msg.data,msg.offset+msg_offset,msg_length);
         				
-        				code = -102;//足包头，不足包体-->不足整包
+        				code = -103;//足包头，不足包体-->不足整包
         				half_packet_count++;
         				break;
             		}
     			}
 			}
     	}else{//说明有分包现象，只接收了部分包，未收到整包
-			int msg_offset = 0;
-			int header_length = DataPacket.getHeaderLength();
+			
+    		int msg_offset = 0;
+			int msg_length = msg.length ;
+			int base_header_length = DataPacket.getBaseHeaderLength();
+			
 			while(true){
-				int the_rest_msglength = msg.length -msg_offset ;
-				if(the_rest_msglength == 0){
-					code = 2;
+
+				if(msg_length == 0){
+					code = 20;
 					break;
 				}
 				
-				if(client.mReceivingMsg.length < header_length){//说明还没有读取完完整的一个包头，继续读取
-    				int remain_header_length = header_length - client.mReceivingMsg.length;
-    				int real_read_remain_header_length = Math.min(remain_header_length, the_rest_msglength);
-    				if(real_read_remain_header_length >0){
-        				System.arraycopy(msg.data,msg.offset+msg_offset,client.mReceivingMsg.data,client.mReceivingMsg.offset+client.mReceivingMsg.length,real_read_remain_header_length);
-        				client.mReceivingMsg.length += real_read_remain_header_length;
-        				msg_offset += real_read_remain_header_length;
-    				}
-    			}
-    			
-    			if(client.mReceivingMsg.length >= header_length){//说明包头读完了，接着读取包体了
-    				int packetLength = DataPacket.getLength(client.mReceivingMsg.data,client.mReceivingMsg.offset);
-    				int remain_packet_length = packetLength - client.mReceivingMsg.length;
-    				int real_read_remain_packet_length = Math.min(remain_packet_length, the_rest_msglength);
-    				if(real_read_remain_packet_length >0){
-        				System.arraycopy(msg.data,msg.offset+msg_offset,client.mReceivingMsg.data,client.mReceivingMsg.offset+client.mReceivingMsg.length,real_read_remain_packet_length);
-        				client.mReceivingMsg.length += real_read_remain_packet_length;
-        				msg_offset += real_read_remain_packet_length;
+				//1.说明还没有读取完完整的一个包头，继续读取
+				if(client.mReceivingMsg.length < base_header_length){
+    				int remain_header_base_length = base_header_length - client.mReceivingMsg.length;
+    				int rr_header_base_length = Math.min(remain_header_base_length, msg_length);//真实读取的数据长度real-read short for rr
+    				if(rr_header_base_length > 0){
+        				System.arraycopy(msg.data,msg.offset+msg_offset,client.mReceivingMsg.data,client.mReceivingMsg.offset+client.mReceivingMsg.length,rr_header_base_length);
+        				
+        				client.mReceivingMsg.length += rr_header_base_length;
+        				
+        				msg_offset += rr_header_base_length;
+        				msg_length -= rr_header_base_length;
     				}
     				
-    				if(client.mReceivingMsg.length == packetLength){//说明包完整了
+    				if(rr_header_base_length < remain_header_base_length){//实际读取的数据量 少于 需要的数据量，说明包读取完了
+        				code = -201;//不足包头
+        				half_packet_count++;
+    					break;
+    				}else if(msg_length == 0){
+        				code = -202;//足包头,但数据读取完了
+        				half_packet_count++;
+    					break;
+    				}
+    			}
+				
+				//2.说明包头读完了，接着读取扩展包头
+				int header_start 	= client.mReceivingMsg.offset;
+				int header_length   = DataPacket.getHeaderLength(client.mReceivingMsg.data, header_start);
+				if(client.mReceivingMsg.length < header_length){
+    				//读取扩展包头
+    				int remain_header_extend_length = header_length - client.mReceivingMsg.length;
+    				int rr_header_extend_length = Math.min(remain_header_extend_length, msg_length);//真实读取的数据长度real-read short for rr
+    				if(rr_header_extend_length > 0){
+        				System.arraycopy(msg.data,msg.offset+msg_offset,client.mReceivingMsg.data,client.mReceivingMsg.offset+client.mReceivingMsg.length,rr_header_extend_length);
+        				
+    					client.mReceivingMsg.length += rr_header_extend_length;
     					
-            			int header_start 	= client.mReceivingMsg.offset;
-            			int body_start 		= header_start + header_length;
-            			int body_length     = DataPacket.getLength(client.mReceivingMsg.data, header_start)-header_length;
+    					msg_offset += rr_header_extend_length;
+        				msg_length -= rr_header_extend_length;
+    				}
+    				
+    				if(rr_header_extend_length < remain_header_extend_length){//实际读取的数据量 少于 需要的数据量，说明包读取完了
+    					code = -203;//不足扩展包头
+        				half_packet_count++;
+    					break;
+    				}else if(msg_length == 0){
+        				code = -204;//足扩展包头,但数据读取完了
+        				half_packet_count++;
+    					break;
+    				}
+				}
+				
+				//3.说明包头,扩展包头读完了，接着读取包体
+    			if(client.mReceivingMsg.length >= header_length){
+    				
+    				int packet_length  	= DataPacket.getLength(client.mReceivingMsg.data,header_start);
+    				//读取包体
+    				int remain_body_length = packet_length - client.mReceivingMsg.length;
+    				int rr_body_length = Math.min(remain_body_length, msg_length);//真实读取的数据长度real-read short for rr
+    				if(rr_body_length >0){
+        				System.arraycopy(msg.data,msg.offset+msg_offset,client.mReceivingMsg.data,client.mReceivingMsg.offset+client.mReceivingMsg.length,rr_body_length);
+        				
+        				client.mReceivingMsg.length += rr_body_length;
+        				
+    					msg_offset += rr_body_length;
+        				msg_length -= rr_body_length;
+    				}
+    				if(rr_body_length < remain_body_length){//实际读取的数据量 少于 需要的数据量，说明包读取完了
+    					code = -205;//不足包体
+        				half_packet_count++;
+    					break;
+    				}
+    				
+    				if(client.mReceivingMsg.length == packet_length){//说明包完整了
+
+            			int body_start 		= header_start 	+ header_length;
+            			int body_length     = packet_length - header_length;
             			
-    					int cmd = DataPacket.getCmd(client.mReceivingMsg.data, header_start);
-            			dispatchMessage(client,cmd,client.mReceivingMsg,header_start,header_length,body_start,body_length);
+            			dispatchMessage(client,client.mReceivingMsg,header_start,header_length,body_start,body_length);
             			
     					full_packet_count++;
     					
     					client.mReceivingMsg.length = 0;
-            			if(msg.length - msg_offset > 0){//说明需要继续接收,还有粘包现象
+    					
+            			if(msg_length > 0){//剩下还有数据,说明还有粘包现象
+            				
+            				//先看看client.mReceivingMsg是否可以复用
+            				if(msg_length >= DataPacket.Header.HEADER_OFFSET_SEQUENCEID){
+            					int next_pkg_length = DataPacket.getLength(msg.data,msg.offset+msg_offset);
+            					if(next_pkg_length > client.mReceivingMsg.capacity){//说明不可以复用
+                    				MessageBuffer.getInstance().release(client.mReceivingMsg);
+                    				client.mReceivingMsg = MessageBuffer.getInstance().buildWithCapacity(next_pkg_length,msg.data,msg.offset+msg_offset,0);//说明不可以复用，以实际容量为准
+            					}else{
+            						continue;//说明可以复用
+            					}
+            				}else{
+            					int next_pkg_length = 16384;
+            					client.mReceivingMsg = MessageBuffer.getInstance().buildWithCapacity(next_pkg_length,msg.data,msg.offset+msg_offset,0);//说明不可以复用，以最大为准
+            				}
+            				
             				continue;
             			}else{//说明可以回收
-            				code = 3;
+            				code = 21;
             				MessageBuffer.getInstance().release(client.mReceivingMsg);
             				client.mReceivingMsg = null;
             				break;
             			}
-    				}else if(client.mReceivingMsg.length < packetLength){//说明包还未完整
-        				code = -202;
+    				}else if(client.mReceivingMsg.length < packet_length){//说明包还未完整 ,//Error------如果逻辑正常，是不会走到这里来的
+        				code = -206;
         				half_packet_count++;
     					break;
-    				}else {//说明异常了，需要重连
-    					code = -203;
+    				}else {//说明异常了，需要重连,//Error------如果逻辑正常，是不会走到这里来的
+    					code = -207;
     					break;
     				}
     			}else{
-    				code = -201;//不足包头
+    				code = -208;//不足包头,//Error------如果逻辑正常，是不会走到这里来的
     				half_packet_count++;
     				break;
     			}
@@ -163,9 +253,9 @@ public class ClientMessageProcessor extends AbstractClientMessageProcessor {
 		Logger.v("output_packet_broadcast cmd 0x" + Integer.toHexString(DataPacket.getCmd(src, offset)) + " length " + length);
 	}
 		
-	 public void dispatchMessage(AbstractClient client, int cmd ,Message msg,int header_start,int header_length,int body_start,int body_length){
-     	try {
-    		
+	public void dispatchMessage(AbstractClient client ,Message msg,int header_start,int header_length,int body_start,int body_length){
+    	try {
+    		int cmd   = DataPacket.getCmd(msg.data, header_start);
     		Logger.v("input_packet cmd 0x" + Integer.toHexString(cmd) + " name " + LoginCmd.getCmdString(cmd) + " length " + DataPacket.getLength(msg.data,header_start));
     		
         	if(cmd == LoginCmd.CMD_LOGIN_REQUEST){
