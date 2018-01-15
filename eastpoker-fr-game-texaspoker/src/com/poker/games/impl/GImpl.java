4cdfe29faa0f7b8652b26cb47e2469455871cce2
Long <longhui.yang@inveno.cn>
2018-01-15 20:45:41 +0800

    子游戏实现

---
+++ eastpoker-fr-game-texaspoker/src/com/poker/games/impl/GImpl.java
@@ -0,0 +1,18 @@
+package com.poker.games.impl;
+
+import com.poker.games.AbstractGame;
+import com.poker.games.User;
+
+/**
+ * 具体游戏的实现类
+ * @author Administrator
+ * 2018年1月15日
+ */
+public class GImpl extends AbstractGame {
+
+	@Override
+	protected void onReceiveCmd(User mUser, int cmd, byte[] data, int offset, int length) {
+		
+	}
+	
+}
