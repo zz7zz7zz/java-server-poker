7de0c1fccdc243909ee146e56b74ad1cd90f4d0c
Long <longhui.yang@inveno.cn>
2018-01-17 09:54:38 +0800

    用户池缓存逻辑

--- eastpoker-fr-game/src/com/poker/games/Room.java
+++ eastpoker-fr-game/src/com/poker/games/Room.java
@@ -7,10 +7,15 @@ public class Room {
 	public Table mTables[];
 	
 	public Room(Config mConfig) {
+		
 		mTables = new Table[mConfig.table_count];
 		for(int i=0;i<mConfig.table_count;i++){
 			mTables[i] = new Table(mConfig.table_max_user);
 		}
+		
+		//预先分配1/4桌子数目的用户，每次增长1/4桌子数目的用户
+		int user_init_size = (int)(0.25*mConfig.table_count) * mConfig.table_max_user;
+		UserPool.init(user_init_size,user_init_size);
 	}
 
 }
--- eastpoker-fr-game/src/com/poker/games/User.java
+++ eastpoker-fr-game/src/com/poker/games/User.java
@@ -2,9 +2,17 @@ package com.poker.games;
 
 public class User {
 	
-	public int uid;//用户唯一Id
+	public long uid;//用户唯一Id
 	public long chip;//用户筹码
 	public int level;//用户等级
 	
 	public int seatId = -1;//座位id
+	
+	public void reset(){
+		uid = 0;
+		chip = 0;
+		level = 0;
+		
+		seatId = 0;
+	}
 }
