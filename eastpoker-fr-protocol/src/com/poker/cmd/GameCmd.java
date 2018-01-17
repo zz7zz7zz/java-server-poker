00915f9b6108170af6465209b12a0d5bfdfa12bf
Long <longhui.yang@inveno.cn>
2018-01-17 14:40:57 +0800

    基本命令字整理

--- eastpoker-fr-protocol/src/com/poker/cmd/GameCmd.java
+++ eastpoker-fr-protocol/src/com/poker/cmd/GameCmd.java
@@ -1,26 +1,28 @@
 package com.poker.cmd;
 
+import com.poker.base.ServerIds;
+
 public final class GameCmd {
 	
 	//-----------------------------------------------------
-	public static int CMD_USER_ENTER 				= 0x1;//用户进入
-	public static int CMD_BROADCAST_USER_ENTER  	= 0x2;
+	public static int CMD_USER_ENTER 				= (ServerIds.SERVER_GAME <<16) + 1;//0x7001用户进入
+	public static int CMD_BROADCAST_USER_ENTER  	= (ServerIds.SERVER_GAME <<16) + 2;//0x7002
 	
-	public static int CMD_USER_EXIT             	= 0x3;//用户退出
-	public static int CMD_BROADCAST_USER_EXIT   	= 0x4;
+	public static int CMD_USER_EXIT             	= (ServerIds.SERVER_GAME <<16) + 3;//0x7003用户退出
+	public static int CMD_BROADCAST_USER_EXIT   	= (ServerIds.SERVER_GAME <<16) + 4;//0x7004
 	
-	public static int CMD_USER_READY 				= 0x5;//用户准备
-	public static int CMD_BROADCAST_USER_READY  	= 0x6;
+	public static int CMD_USER_READY 				= (ServerIds.SERVER_GAME <<16) + 5;//0x7005用户准备
+	public static int CMD_BROADCAST_USER_READY  	= (ServerIds.SERVER_GAME <<16) + 6;//0x7006
 	
-	public static int CMD_USER_OFFLINE     		= 0x7;//用户掉线
-	public static int CMD_BROADCAST_USER_OFFLINE 	= 0x8;
+	public static int CMD_USER_OFFLINE     			= (ServerIds.SERVER_GAME <<16) + 7;//0x7007用户掉线
+	public static int CMD_BROADCAST_USER_OFFLINE 	= (ServerIds.SERVER_GAME <<16) + 8;//0x7008
 	
 	//-----------------------------------------------------
-	public static int CMD_KICK_USER 	    		= 0x9;//用户踢人
-	public static int CMD_BROADCAST_KICK_USER		= 0xA;
+	public static int CMD_KICK_USER 	    		= (ServerIds.SERVER_GAME <<16) + 9;//0x7009用户踢人
+	public static int CMD_BROADCAST_KICK_USER		= (ServerIds.SERVER_GAME <<16) + 0xA;//0x700A
 	
-	public static int CMD_ONLOOKERS     		    = 0xB;//用户申请围观
-	public static int CMD_BROADCAST_ONLOOKERS 	    = 0xC;
+	public static int CMD_ONLOOKERS     		    = (ServerIds.SERVER_GAME <<16) + 0xB;//0x700B用户申请围观
+	public static int CMD_BROADCAST_ONLOOKERS 	    = (ServerIds.SERVER_GAME <<16) + 0xC;//0x700C
 	
 	//-----------------------------------------------------
 	
