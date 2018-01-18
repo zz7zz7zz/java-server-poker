7f3dbbe2c7c00084a3a75c1757b2007754ed7d80
Long <longhui.yang@inveno.cn>
2018-01-18 15:37:39 +0800

    更新命令字的值

--- eastpoker-fr-game-texaspoker/src/com/poker/games/impl/handler/GCmd.java
+++ eastpoker-fr-game-texaspoker/src/com/poker/games/impl/handler/GCmd.java
@@ -2,12 +2,22 @@ package com.poker.games.impl.handler;
 
 public class GCmd {
 	
-	public int CMD_GAME_START   		= 0x1001; //游戏开始
-	public int CMD_DEAL   				= 0x1002; //发牌，意味着牌局开始
-	public int CMD_ACTION 				= 0x1003; //操作
-	public int CMD_BROADCAST_ACTION 	= 0x1004; //广播操作
-	public int CMD_SHOW_HAND 			= 0x1005; //秀牌
-	public int CMD_BROADCAST_SHOW_HAND 	= 0x1006; //广播秀牌
-	public int CMD_ALL_SHOW_HAND 		= 0x1007; //集体秀牌
-	public int CMD_GAME_END 	    	= 0x1008; //游戏结束，进行结算
+	//客户端发起命令字
+	public int CMD_CLIENT_WHO_ACTION_WHAT				= 0x1006; //谁做了什么操作
+	public int CMD_CLIENT_SHOW_HAND 					= 0x1007; //谁秀牌
+	
+	public int CMD_CLIENT_RECONNECT 					= 0x1009; //客户端数据不对，请求重连
+	
+	//客户端返回命令字
+	public int CMD_SERVER_GAME_START   					= 0x2001; //游戏开始，下发相关配置
+	public int CMD_SERVER_DEAL_PREFLOP   				= 0x2002; //发底牌
+	public int CMD_SERVER_DEAL_FLOP  					= 0x2003; //发翻牌
+	public int CMD_SERVER_DEAL_TURN   					= 0x2004; //发转牌
+	public int CMD_SERVER_DEAL_RIVER   					= 0x2005; //发河牌
+
+	public int CMD_SERVER_BROADCAST_WHO_ACTION_WAHT 	= 0x2006; //广播 谁做了什么操作,下一个操作者是谁
+	public int CMD_SERVER_BROADCAST_SHOW_HAND 			= 0x2007; //广播 谁秀牌
+	public int CMD_SERVER_BROADCAST_ALL_SHOW_HAND 		= 0x2008; //广播 集体秀牌
+
+	public int CMD_SERVER_GAME_END 	    				= 0x2009; //游戏结束，进行结算
 }
