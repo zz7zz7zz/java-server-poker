e8c890b64de482becf49093390c1f80d39db87f5
Long <longhui.yang@inveno.cn>
2018-01-18 09:45:57 +0800

    用户昵称

--- eastpoker-fr-game/src/com/poker/games/User.java
+++ eastpoker-fr-game/src/com/poker/games/User.java
@@ -3,7 +3,7 @@ package com.poker.games;
 public class User {
 	
 	public long uid;//用户唯一Id
-	
+	public String nick_name;//用户昵称
 	public String head_portrait;//头像
 	public long chip;//用户筹码
 	public int level;//用户等级
