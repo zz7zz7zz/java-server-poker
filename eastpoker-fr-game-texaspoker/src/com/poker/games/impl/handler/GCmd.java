2bc9df5226da6e8fee10d551f30598f2b99247e6
Long <longhui.yang@inveno.cn>
2018-01-18 15:40:00 +0800

    更新命令字

--- eastpoker-fr-game-texaspoker/src/com/poker/games/impl/handler/GCmd.java
+++ eastpoker-fr-game-texaspoker/src/com/poker/games/impl/handler/GCmd.java
@@ -5,11 +5,11 @@ public class GCmd {
 	//客户端发起命令字
 	public int CMD_CLIENT_WHO_ACTION_WHAT				= 0x1006; //谁做了什么操作
 	public int CMD_CLIENT_SHOW_HAND 					= 0x1007; //谁秀牌
-	
 	public int CMD_CLIENT_RECONNECT 					= 0x1009; //客户端数据不对，请求重连
 	
 	//客户端返回命令字
 	public int CMD_SERVER_GAME_START   					= 0x2001; //游戏开始，下发相关配置
+	
 	public int CMD_SERVER_DEAL_PREFLOP   				= 0x2002; //发底牌
 	public int CMD_SERVER_DEAL_FLOP  					= 0x2003; //发翻牌
 	public int CMD_SERVER_DEAL_TURN   					= 0x2004; //发转牌
@@ -19,5 +19,7 @@ public class GCmd {
 	public int CMD_SERVER_BROADCAST_SHOW_HAND 			= 0x2007; //广播 谁秀牌
 	public int CMD_SERVER_BROADCAST_ALL_SHOW_HAND 		= 0x2008; //广播 集体秀牌
 
-	public int CMD_SERVER_GAME_END 	    				= 0x2009; //游戏结束，进行结算
+	public int CMD_SERVER_RECONNECT 	    			= 0x2009; //客户端请求重连
+	
+	public int CMD_SERVER_GAME_END 	    				= 0x2010; //游戏结束，进行结算
 }
