9b22b894ec5b39a48a1ffa19e7eba210690c8697
Long <longhui.yang@inveno.cn>
2018-01-22 16:45:17 +0800

    更新数据

--- eastpoker-fr-game-texaspoker/src/com/poker/games/impl/GTable.java
+++ eastpoker-fr-game-texaspoker/src/com/poker/games/impl/GTable.java
@@ -1,6 +1,7 @@
 package com.poker.games.impl;
 
 import com.poker.games.Table;
+import com.poker.games.User;
 public class GTable extends Table {
 
 	public GTable(int tableId, int table_max_user) {
@@ -8,12 +9,48 @@ public class GTable extends Table {
 	}
 
 	@Override
-	protected int dispatchTableMessage(int cmd, byte[] data, int header_start, int header_length, int body_start,
-			int body_length) {
+	protected int onTableUserFirstLogin(User mUser) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
 
+
+	@Override
+	protected int onTableUserReLogin(User mUser) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+	@Override
+	protected int onTableUserExit(User mUser) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+	@Override
+	protected int onTableUserOffline(User mUser) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+	@Override
+	protected int onTableUserReady(User mUser) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	@Override
+	protected int dispatchTableMessage(int cmd, byte[] data, int header_start, int header_length, int body_start,
+			int body_length) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
