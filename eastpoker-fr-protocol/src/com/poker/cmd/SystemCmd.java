28f3d9e15402b0502af36086bcd301b2a5332fc8
Long <longhui.yang@inveno.cn>
2018-01-23 19:57:27 +0800

    sssss

---
+++ eastpoker-fr-protocol/src/com/poker/cmd/SystemCmd.java
@@ -0,0 +1,12 @@
+package com.poker.cmd;
+
+public class SystemCmd {
+	
+	public static final int CMD_HEART_BEATE 	= 0x0001;
+	public static final int CMD_UPDATE_CONFIG 	= 0x0002;
+	
+	public static final int CMD_ERR 			= 0x000A;
+	
+	//错误类型
+	public static final int ERR_CODE_LOGIN_FAILED_TABLE_FULL = 100;//登录游戏错误
+}
