a5c73f061106116cbbf56505c6d6bb8ed8a7e7da
Long <longhui.yang@inveno.cn>
2018-01-26 18:09:18 +0800

    更新协议

--- eastpoker-fr-protocol/src/com/poker/data/DataTransfer.java
+++ eastpoker-fr-protocol/src/com/poker/data/DataTransfer.java
@@ -3,40 +3,39 @@ package com.poker.data;
 
 import com.google.protobuf.ByteString;
 import com.poker.base.ServerIds;
-import com.poker.cmd.DispatchCmd;
 import com.poker.protocols.server.DispatchChainProto;
 import com.poker.protocols.server.DispatchPacketProto;
 
 public final class DataTransfer {
 
 	//---------------------------------------------------------------------------------------------------
-	public static int send2Access(byte[] writeBuff,int squenceId, byte[] data, int offset , int length,int src_server_type , int src_server_id , int dst_server_id,int gameGroup,int matchGroup){
-		return send2Dispatcher(writeBuff,squenceId, DispatchCmd.CMD_DISPATCH_DATA, data, offset, length, src_server_type, src_server_id, ServerIds.SERVER_ACCESS, dst_server_id,gameGroup,matchGroup);
+	public static int send2Access(byte[] writeBuff,int squenceId,long uid, int cmd ,int dispatch_type, byte[] data, int offset , int length,int src_server_type , int src_server_id , int dst_server_id,int gameGroup,int matchGroup){
+		return send2Dispatcher(writeBuff,squenceId, uid,cmd,dispatch_type, data, offset, length, src_server_type, src_server_id, ServerIds.SERVER_ACCESS, dst_server_id,gameGroup,matchGroup);
 	}
 	
-	public static int send2Login(byte[] writeBuff,int squenceId, byte[] data, int offset , int length,int src_server_type , int src_server_id , int dst_server_id,int gameGroup,int matchGroup){
-		return send2Dispatcher(writeBuff,squenceId, DispatchCmd.CMD_DISPATCH_DATA, data, offset, length, src_server_type, src_server_id, ServerIds.SERVER_LOGIN, dst_server_id,gameGroup,matchGroup);
+	public static int send2Login(byte[] writeBuff,int squenceId,long uid, int cmd ,int dispatch_type, byte[] data, int offset , int length,int src_server_type , int src_server_id , int dst_server_id,int gameGroup,int matchGroup){
+		return send2Dispatcher(writeBuff,squenceId, uid,cmd,dispatch_type, data, offset, length, src_server_type, src_server_id, ServerIds.SERVER_LOGIN, dst_server_id,gameGroup,matchGroup);
 	}
 	
-	public static int send2User(byte[] writeBuff,int squenceId,  byte[] data, int offset , int length,int src_server_type , int src_server_id , int dst_server_id,int gameGroup,int matchGroup){
-		return send2Dispatcher(writeBuff,squenceId, DispatchCmd.CMD_DISPATCH_DATA, data, offset, length,  src_server_type, src_server_id, ServerIds.SERVER_USER, dst_server_id,gameGroup,matchGroup);
+	public static int send2User(byte[] writeBuff,int squenceId,long uid, int cmd ,int dispatch_type,  byte[] data, int offset , int length,int src_server_type , int src_server_id , int dst_server_id,int gameGroup,int matchGroup){
+		return send2Dispatcher(writeBuff,squenceId, uid,cmd,dispatch_type, data, offset, length,  src_server_type, src_server_id, ServerIds.SERVER_USER, dst_server_id,gameGroup,matchGroup);
 	}
 	
-	public static int send2Allocator(byte[] writeBuff,int squenceId, byte[] data, int offset , int length,int src_server_type , int src_server_id , int dst_server_id,int gameGroup,int matchGroup){
-		return send2Dispatcher(writeBuff,squenceId, DispatchCmd.CMD_DISPATCH_DATA, data, offset, length,  src_server_type, src_server_id, ServerIds.SERVER_ALLOCATOR, dst_server_id,gameGroup,matchGroup);
+	public static int send2Allocator(byte[] writeBuff,int squenceId,long uid, int cmd ,int dispatch_type, byte[] data, int offset , int length,int src_server_type , int src_server_id , int dst_server_id,int gameGroup,int matchGroup){
+		return send2Dispatcher(writeBuff,squenceId, uid,cmd,dispatch_type, data, offset, length,  src_server_type, src_server_id, ServerIds.SERVER_ALLOCATOR, dst_server_id,gameGroup,matchGroup);
 	}
 	
-	public static int send2Gamer(byte[] writeBuff,int squenceId, byte[] data, int offset , int length,int src_server_type , int src_server_id , int dst_server_id,int gameGroup,int matchGroup){
-		return send2Dispatcher(writeBuff,squenceId, DispatchCmd.CMD_DISPATCH_DATA, data, offset, length,  src_server_type, src_server_id, ServerIds.SERVER_GAME, dst_server_id,gameGroup,matchGroup);
+	public static int send2Gamer(byte[] writeBuff,int squenceId,long uid, int cmd ,int dispatch_type, byte[] data, int offset , int length,int src_server_type , int src_server_id , int dst_server_id,int gameGroup,int matchGroup){
+		return send2Dispatcher(writeBuff,squenceId, uid,cmd,dispatch_type, data, offset, length,  src_server_type, src_server_id, ServerIds.SERVER_GAME, dst_server_id,gameGroup,matchGroup);
 	}
 	
-	public static int send2GoldCoin(byte[] writeBuff,int squenceId, byte[] data, int offset , int length,int src_server_type , int src_server_id , int dst_server_id,int gameGroup,int matchGroup){
-		return send2Dispatcher(writeBuff,squenceId, DispatchCmd.CMD_DISPATCH_DATA, data, offset, length,  src_server_type, src_server_id, ServerIds.SERVER_GOLDCOIN, dst_server_id,gameGroup,matchGroup);
+	public static int send2GoldCoin(byte[] writeBuff,int squenceId,long uid, int cmd ,int dispatch_type, byte[] data, int offset , int length,int src_server_type , int src_server_id , int dst_server_id,int gameGroup,int matchGroup){
+		return send2Dispatcher(writeBuff,squenceId, uid,cmd,dispatch_type, data, offset, length,  src_server_type, src_server_id, ServerIds.SERVER_GOLDCOIN, dst_server_id,gameGroup,matchGroup);
 	}
 	
 	//---------------------------------------------------------------------------------------------------
 	//转发
-	public static int send2Dispatcher(byte[] writeBuff,int squenceId, int cmd , byte[] data,int offset,int length, int src_server_type , int src_server_id ,int dst_server_type , int dst_server_id,int gameGroup,int matchGroup){
+	public static int send2Dispatcher(byte[] writeBuff,int squenceId,long uid, int cmd ,int dispatch_type, byte[] data,int offset,int length, int src_server_type , int src_server_id ,int dst_server_type , int dst_server_id,int gameGroup,int matchGroup){
 		
 		//流水id
 		DispatchPacketProto.DispatchPacket.Builder builder = DispatchPacketProto.DispatchPacket.newBuilder();
@@ -48,6 +47,19 @@ public final class DataTransfer {
 		chainBuilder.setSrcServerId(src_server_id);
 		chainBuilder.setDstServerType(dst_server_type);
 		chainBuilder.setDstServerId(dst_server_id);
+		
+		if(uid !=0){
+			chainBuilder.setUid(uid);
+		}
+
+		if(cmd !=0){
+			chainBuilder.setCmd(cmd);
+		}
+		
+		if(dispatch_type !=0){
+			chainBuilder.setDispatchType(dispatch_type);
+		}
+		
 		if(-1 != gameGroup){
 			chainBuilder.setDstGameGroup(gameGroup);
 		}
