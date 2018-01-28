db0abdbd1069e73574e6af2b36023b6adc953c9b
zz7zz7zz <zz7zz7zz.zz@hotmail.com>
2018-01-28 11:21:46 +0800

    EEEE

---
+++ eastpoker-fr-access/src/com/poker/access/object/User.java
@@ -0,0 +1,27 @@
+package com.poker.access.object;
+
+public class User {
+	
+	public long uid;
+	
+	public int  socketId;
+	public long socketHeartBeatTime;
+	
+	public int gameSid;
+	public int gameId;
+	
+	public int matchSid;
+	public int matchId;
+	
+	public void reset(){
+		uid = 0;
+		socketId = 0;
+		socketHeartBeatTime = 0;
+		
+		gameSid = 0;
+		gameId = 0;
+		
+		matchSid = 0;
+		matchId = 0;
+	}
+}
