7f882f7c8a8451afa8dfe66fae3334754d05a9a0
Long <longhui.yang@inveno.cn>
2018-01-29 11:56:33 +0800

    007

--- eastpoker-fr-access/src/com/poker/access/packet/PacketTransfer.java
+++ eastpoker-fr-access/src/com/poker/access/packet/PacketTransfer.java
@@ -1,6 +1,10 @@
 package com.poker.access.packet;
 
-import com.poker.data.DataTransfer;
+import com.google.protobuf.ByteString;
+import com.poker.base.ServerIds;
+import com.poker.data.DataPacket;
+import com.poker.protocols.server.DispatchChainProto;
+import com.poker.protocols.server.DispatchPacketProto;
 
 public class PacketTransfer{
 	
@@ -14,11 +18,53 @@ public class PacketTransfer{
 	
 	public static int send2Login(byte[] writeBuff,int squenceId,long uid, int cmd ,int dispatch_type, byte[] data, int offset ,int length){
 		int dst_server_id = 0;
-		return DataTransfer.send2Login(writeBuff,squenceId,uid,cmd,dispatch_type,data,offset,length, SERVER_TYPE, SERVER_ID, dst_server_id,-1,-1);
+		return send2Dispatcher(writeBuff,squenceId, uid,cmd,dispatch_type, data, offset, length, SERVER_TYPE, SERVER_ID, ServerIds.SERVER_LOGIN, dst_server_id,-1,-1);
 	}
 	
 	public static int send2User(byte[] writeBuff,int squenceId ,long uid, int cmd ,int dispatch_type, byte[] data, int offset ,int length){
 		int dst_server_id = 0;
-		return DataTransfer.send2User(writeBuff,squenceId,uid,cmd,dispatch_type, data,offset,length, SERVER_TYPE, SERVER_ID, dst_server_id,-1,-1);
+		return send2Dispatcher(writeBuff,squenceId, uid,cmd,dispatch_type, data, offset, length, SERVER_TYPE, SERVER_ID, ServerIds.SERVER_USER, dst_server_id,-1,-1);
+	}
+	
+	public static int send2Dispatcher(byte[] writeBuff,int squenceId,long uid, int cmd ,int dispatch_type, byte[] data,int offset,int length, int src_server_type , int src_server_id ,int dst_server_type , int dst_server_id,int gameGroup,int matchGroup){
+		
+		//流水id
+		DispatchPacketProto.DispatchPacket.Builder builder = DispatchPacketProto.DispatchPacket.newBuilder();
+		builder.setSequenceId(squenceId);
+		
+		//调用链
+		DispatchChainProto.DispatchChain.Builder chainBuilder = DispatchChainProto.DispatchChain.newBuilder();
+		chainBuilder.setSrcServerType(src_server_type);
+		chainBuilder.setSrcServerId(src_server_id);
+		chainBuilder.setDstServerType(dst_server_type);
+		chainBuilder.setDstServerId(dst_server_id);
+		
+		if(uid !=0){
+			chainBuilder.setUid(uid);
+		}
+
+		if(cmd !=0){
+			chainBuilder.setCmd(cmd);
+		}
+		
+		if(dispatch_type !=0){
+			chainBuilder.setDispatchType(dispatch_type);
+		}
+		
+		if(-1 != gameGroup){
+			chainBuilder.setDstGameGroup(gameGroup);
+		}
+		if(-1 != matchGroup){
+			chainBuilder.setDstMatchGroup(matchGroup);
+		}
+		builder.addDispatchChainList(chainBuilder);
+		
+		//转发的数据
+		builder.setData(ByteString.copyFrom(data,offset,length));
+		
+		DispatchPacketProto.DispatchPacket dispatchPacket = builder.build();
+		byte[] body = dispatchPacket.toByteArray();
+		
+		return DataPacket.write(writeBuff, squenceId, cmd, (byte)0, body,0,body.length);
 	}
 }
