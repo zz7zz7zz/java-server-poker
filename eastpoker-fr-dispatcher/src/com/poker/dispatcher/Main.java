f6768537472440c2e4c4f289dbac6ec533868502
Long <longhui.yang@inveno.cn>
2018-01-30 18:02:35 +0800

    008

--- eastpoker-fr-dispatcher/src/com/poker/dispatcher/Main.java
+++ eastpoker-fr-dispatcher/src/com/poker/dispatcher/Main.java
@@ -13,6 +13,7 @@ import com.open.util.log.Logger;
 import com.open.util.log.base.LogConfig;
 import com.poker.base.ServerIds;
 import com.poker.common.config.Config;
+import com.poker.common.packet.PacketTransfer;
 import com.poker.data.DataPacket;
 import com.poker.dispatcher.handler.ServerHandler;
 import com.poker.packet.InPacket;
@@ -98,7 +99,7 @@ public class Main {
     
     //---------------------------------------初始化全局对象----------------------------------------------------
     private static void initGlobalFields(int packet_max_length_tcp){
-
+    	PacketTransfer.init(libArgsConfig.server_type, libArgsConfig.id);
     }
     
   //---------------------------------------Monitor----------------------------------------------------
