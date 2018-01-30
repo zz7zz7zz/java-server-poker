734d47e9b120e64960e490507c898e855c945e16
Long <longhui.yang@inveno.cn>
2018-01-30 17:17:31 +0800

    001

--- eastpoker-fr-access/src/com/poker/access/handler/ServerHandler.java
+++ eastpoker-fr-access/src/com/poker/access/handler/ServerHandler.java
@@ -2,6 +2,8 @@ package com.poker.access.handler;
 
 import com.open.net.server.object.AbstractServerClient;
 import com.open.util.log.Logger;
+import com.poker.access.Main;
+import com.poker.access.object.User;
 import com.poker.base.ServerIds;
 import com.poker.cmd.DispatchCmd;
 import com.poker.common.packet.PacketTransfer;
@@ -28,23 +30,32 @@ public class ServerHandler extends AbsServerHandler{
       		int length = 0;
       		if(server == ServerIds.SERVER_LOGIN){
       			
+      			User user = (User)client.getAttachment();
+      			long uid = null != user ? user.uid : 0;
+      			
       			mOutPacket.begin(squenceId, cmd);
-      			mOutPacket.writeLong(client.mClientId);//额外的数据
+      			mOutPacket.writeLong(client.mClientId);//socketId
       			mOutPacket.writeBytes(data,header_start,header_length+body_length);//原始数据
       			mOutPacket.end();
       			
       			//当InPacket不需要使用时，可以复用buff，防止过多的分配内存，产生内存碎片
       			byte[] mTempBuff = mInPacket.getPacket();
-      			length = PacketTransfer.send2Login(mTempBuff, squenceId, 0,cmd,DistapchType.TYPE_P2P,mOutPacket.getPacket(),0,  mOutPacket.getLength());
+      			length = PacketTransfer.send2Login(mTempBuff, squenceId, uid,cmd,DistapchType.TYPE_P2P,mOutPacket.getPacket(),0,  mOutPacket.getLength());
           		send2Dispatch(mTempBuff,0,length);
           		
       		}else if(server == ServerIds.SERVER_USER){
+      			User user = (User)client.getAttachment();
+      			long uid = null != user ? user.uid : 0;
+      			
+      			mOutPacket.begin(squenceId, cmd);
+      			mOutPacket.writeInt(Main.libArgsConfig.id);//AccessId
+      			mOutPacket.writeBytes(data,header_start,header_length+body_length);//原始数据
+      			mOutPacket.end();
       			
       			//当InPacket不需要使用时，可以复用buff，防止过多的分配内存，产生内存碎片
       			byte[] mTempBuff = mInPacket.getPacket();
-      			length = PacketTransfer.send2User(mTempBuff, squenceId, 0,cmd,DistapchType.TYPE_P2P,data,body_start,body_length);
+      			length = PacketTransfer.send2User(mTempBuff, squenceId, uid,cmd,DistapchType.TYPE_P2P,mOutPacket.getPacket(),0,  mOutPacket.getLength());
           		send2Dispatch(mTempBuff,0,length);
-          		
       		}
       		
 
