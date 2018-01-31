2371707ee78407ced4b437d697dac9b420fe014b
Long <longhui.yang@inveno.cn>
2018-01-31 14:52:53 +0800

    06

--- eastpoker-fr-protocol/src/com/poker/cmd/GameCmd.java
+++ eastpoker-fr-protocol/src/com/poker/cmd/GameCmd.java
@@ -5,7 +5,7 @@ import com.poker.base.ServerIds;
 public final class GameCmd {
 	
 	//-----------------------------------------------------
-	
+	public static int CMD_CHECK_GAME_STATUS 		= (ServerIds.SERVER_GAME <<16) + 0x1100;//0x71001登录游戏
 	public static int CMD_LOGIN_GAME 				= (ServerIds.SERVER_GAME <<16) + 0x1001;//0x71001登录游戏
 	
 	public static int CMD_USER_ENTER 				= (ServerIds.SERVER_GAME <<16) + 0x1002;//0x71001用户进入
