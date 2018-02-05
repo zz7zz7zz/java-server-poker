29dde331bc44e6bfd9ea771d37346044ed73b747
Long <longhui.yang@inveno.cn>
2018-02-05 13:30:03 +0800

    009

--- eastpoker-fr-game/src/com/poker/games/protocols/BaseGameCmd.java
+++ eastpoker-fr-game/src/com/poker/games/protocols/BaseGameCmd.java
@@ -4,10 +4,10 @@ package com.poker.games.protocols;
 public final class BaseGameCmd {
 	
 	//客户端发起   （游戏基础命令字）,范围是0x1001~0x1FFF
-	public static final int CMD_CLIENT_USER_EXIT             	= 0x1001;//用户退出
-	public static final int CMD_CLIENT_USER_READY 				= 0x1002;//用户准备
-	public static final int CMD_CLIENT_KICK_USER 	    		= 0x1003;//用户踢人
-	public static final int CMD_CLIENT_OFFLINE 	    			= 0x1004;//用户掉线
+	public static final int CMD_CLIENT_USER_EXIT             	= 0x1002;//用户退出
+	public static final int CMD_CLIENT_USER_READY 				= 0x1003;//用户准备
+	public static final int CMD_CLIENT_KICK_USER 	    		= 0x1004;//用户踢人
+	public static final int CMD_CLIENT_OFFLINE 	    			= 0x1005;//用户掉线
 	
 	//服务器返回  （游戏基础命令字）,范围是0x2001~0x2FFF
 	public static final int CMD_SERVER_USERLOGIN   				= 0x2001;//用户进入
