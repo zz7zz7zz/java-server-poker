8b4196a571edfa8b3cfa62b6d0dbc6397fc26e16
Long <longhui.yang@inveno.cn>
2018-02-24 15:34:14 +0800

    001

--- eastpoker-fr-game-texaspoker/src/com/poker/protocols/TexasGameClient.java
+++ eastpoker-fr-game-texaspoker/src/com/poker/protocols/TexasGameClient.java
@@ -1,15 +1,14 @@
 package com.poker.protocols;
 
-import com.poker.games.impl.define.TexasDefine.Operate;
 import com.poker.protocols.texaspoker.TexasGameActionRequestProto.TexasGameActionRequest;
 
 
 public final class TexasGameClient {
 	
 	//用户执行操作
-	public final static byte[] action(Operate operate,long chip){
+	public final static byte[] action(int operate,long chip){
 		TexasGameActionRequest.Builder builder = TexasGameActionRequest.newBuilder();
-		builder.setOperate(operate.getValue());
+		builder.setOperate(operate);
 		builder.setChip(chip);
 		byte[] body = builder.build().toByteArray();
 		return body;
