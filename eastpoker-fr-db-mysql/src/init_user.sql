b3c5111b0b593efc3e21d33495c69b806aa4ad35
Long <longhui.yang@inveno.cn>
2018-04-02 09:18:11 +0800

    更新表字段

--- eastpoker-fr-db-mysql/src/init_user.sql
+++ eastpoker-fr-db-mysql/src/init_user.sql
@@ -1,61 +1,81 @@
 use eastpoker;
 create table if not exists user0(
 	uid BIGINT NOT NULL PRIMARY KEY,
+	uuid VARCHAR(32),
+	password VARCHAR(32),
 	name VARCHAR(32),
 	portrait VARCHAR(128),
-	reg_time TIMESTAMP NOT NULL
+	regTime TIMESTAMP NOT NULL
 );
 create table if not exists user1(
 	uid BIGINT NOT NULL PRIMARY KEY,
+	uuid VARCHAR(32),
+	password VARCHAR(32),
 	name VARCHAR(32),
 	portrait VARCHAR(128),
-	reg_time TIMESTAMP NOT NULL
+	regTime TIMESTAMP NOT NULL
 );
 create table if not exists user2(
 	uid BIGINT NOT NULL PRIMARY KEY,
+	uuid VARCHAR(32),
+	password VARCHAR(32),
 	name VARCHAR(32),
 	portrait VARCHAR(128),
-	reg_time TIMESTAMP NOT NULL
+	regTime TIMESTAMP NOT NULL
 );
 create table if not exists user3(
 	uid BIGINT NOT NULL PRIMARY KEY,
+	uuid VARCHAR(32),
+	password VARCHAR(32),
 	name VARCHAR(32),
 	portrait VARCHAR(128),
-	reg_time TIMESTAMP NOT NULL
+	regTime TIMESTAMP NOT NULL
 );
 create table if not exists user4(
 	uid BIGINT NOT NULL PRIMARY KEY,
+	uuid VARCHAR(32),
+	password VARCHAR(32),
 	name VARCHAR(32),
 	portrait VARCHAR(128),
-	reg_time TIMESTAMP NOT NULL
+	regTime TIMESTAMP NOT NULL
 );
 create table if not exists user5(
 	uid BIGINT NOT NULL PRIMARY KEY,
+	uuid VARCHAR(32),
+	password VARCHAR(32),
 	name VARCHAR(32),
 	portrait VARCHAR(128),
-	reg_time TIMESTAMP NOT NULL
+	regTime TIMESTAMP NOT NULL
 );
 create table if not exists user6(
 	uid BIGINT NOT NULL PRIMARY KEY,
+	uuid VARCHAR(32),
+	password VARCHAR(32),
 	name VARCHAR(32),
 	portrait VARCHAR(128),
-	reg_time TIMESTAMP NOT NULL
+	regTime TIMESTAMP NOT NULL
 );
 create table if not exists user7(
 	uid BIGINT NOT NULL PRIMARY KEY,
+	uuid VARCHAR(32),
+	password VARCHAR(32),
 	name VARCHAR(32),
 	portrait VARCHAR(128),
-	reg_time TIMESTAMP NOT NULL
+	regTime TIMESTAMP NOT NULL
 );
 create table if not exists user8(
 	uid BIGINT NOT NULL PRIMARY KEY,
+	uuid VARCHAR(32),
+	password VARCHAR(32),
 	name VARCHAR(32),
 	portrait VARCHAR(128),
-	reg_time TIMESTAMP NOT NULL
+	regTime TIMESTAMP NOT NULL
 );
 create table if not exists user9(
 	uid BIGINT NOT NULL PRIMARY KEY,
+	uuid VARCHAR(32),
+	password VARCHAR(32),
 	name VARCHAR(32),
 	portrait VARCHAR(128),
-	reg_time TIMESTAMP NOT NULL
+	regTime TIMESTAMP NOT NULL
 );
