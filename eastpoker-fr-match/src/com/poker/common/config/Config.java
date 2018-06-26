c601c5374258b4127b4dafe638de7d1de0fde6a5
Long <longhui.yang@inveno.cn>
2018-06-26 15:32:06 +0800

    添加Match 服务

---
+++ eastpoker-fr-match/src/com/poker/common/config/Config.java
@@ -0,0 +1,76 @@
+package com.poker.common.config;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+import com.open.net.base.util.CfgParser;
+import com.open.net.client.object.TcpAddress;
+import com.open.net.client.object.UdpAddress;
+
+
+public class Config {
+
+	//Allocator
+	public int game_id;
+	
+	//Dispatcher
+    public TcpAddress[] dispatcher_net_tcp;
+    public UdpAddress[] dispatcher_net_udp;
+    
+    //Monitor
+    public UdpAddress[] monitor_net_udp;
+    
+    //解析文件配置参数
+    public final void initFileConfig(String config_path) {
+        HashMap<String,Object> map = CfgParser.parseToMap(config_path);
+        initFileConfig(map);
+    }
+    
+    //-------------------------------------------------------------------------------------------
+    protected void initFileConfig(HashMap<String,Object> map){
+    	if(null !=map){
+    		
+    		game_id = CfgParser.getInt(map, "Allocator","game_id");
+    		
+            String val[]     = CfgParser.getStringArray(map,"Dispatcher","net_tcp");
+            if(null != val){
+            	dispatcher_net_tcp = new TcpAddress[val.length];
+                for (int i = 0; i < val.length; i++) {
+                    String[] v = val[i].split(":");
+                    if(v.length>1){
+                    	dispatcher_net_tcp[i]  = new TcpAddress(v[0],Integer.valueOf(v[1]));
+                    }
+                }
+            }
+            
+            val              = CfgParser.getStringArray(map,"Dispatcher","net_udp");
+            if(null != val){
+            	dispatcher_net_udp = new UdpAddress[val.length];
+                for (int i = 0; i < val.length; i++) {
+                    String[] v = val[i].split(":");
+                    if(v.length>1){
+                    	dispatcher_net_udp[i] = new UdpAddress(v[0],Integer.valueOf(v[1]));
+                    }
+                }
+            }
+            
+            val              = CfgParser.getStringArray(map,"Monitor","net_udp");
+            if(null != val){
+            	monitor_net_udp = new UdpAddress[val.length];
+                for (int i = 0; i < val.length; i++) {
+                    String[] v = val[i].split(":");
+                    if(v.length>1){
+                    	monitor_net_udp[i] = new UdpAddress(v[0],Integer.valueOf(v[1]));
+                    }
+                }
+            }
+       }
+    }
+
+	@Override
+	public String toString() {
+		return "Config [game_id=" + game_id + ", dispatcher_net_tcp=" + Arrays.toString(dispatcher_net_tcp)
+				+ ", dispatcher_net_udp=" + Arrays.toString(dispatcher_net_udp) + ", monitor_net_udp="
+				+ Arrays.toString(monitor_net_udp) + "]";
+	}
+}
---
+++ eastpoker-fr-match/start.sh
@@ -0,0 +1,3 @@
+nohup java -jar Match.jar -n 'Match' -i 500 -p 10060 > Match_0.out &
+sleep 3
+
